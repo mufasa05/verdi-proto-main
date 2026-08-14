283787782404d87701f25c72b07ec89b7dc63b28
mufasa05 <princeashumba@gmail.com>
2026-08-14 11:26:37 +0200

    Expand marketplace demo products to 84 items with verified image matching and update web build

--- verdi-proto-main/build/web/flutter_bootstrap.js
+++ verdi-proto-main/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2683854391" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4256622541" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
